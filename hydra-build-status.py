9b89b1a23149aa82b65076c3f60edbb6be3040f8
Ivan Mincik <ivan.mincik@gmail.com>
2024-02-02 12:58:23 +0100

    hydra-build-status.py: align output, add URL, pin pkgs versions

--- hydra-build-status.py
+++ hydra-build-status.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i python3 -p python3Packages.requests python3Packages.beautifulsoup4
+#! nix-shell -i python3 -p python3Packages.requests python3Packages.beautifulsoup4 -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/7790e078f8979a9fcd543f9a47427eeaba38f268.tar.gz 
 
 # Report last Hydra build status for given packages. Fail with exit code 1 if
 # status of any given package is not success.
@@ -28,7 +28,7 @@ for pkg in pkgs:
     build_result = build_results[1].find_all("td")
     build_status = build_result[0].img["alt"]
 
-    print(f"{pkg} - {build_status}")
+    print(f"{pkg : <50} {build_status : <20} URL: {URL : <50}")
 
     if build_status != "Succeeded": exit_code = 1
 
