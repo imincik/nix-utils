27d0e65affa450f5381425af2b07d4c3aad21515
Ivan Mincik <ivan.mincik@gmail.com>
2025-05-03 06:42:56 +0200

    hydra-build-status.py: update code formatting

--- maintainer-packages.nix
+++ maintainer-packages.nix
@@ -29,8 +29,9 @@ let
   myMaintainer = pkgs.lib.maintainers.${maintainer};
 
   isMaintainedBy = pkg:
-    elem myMaintainer (pkg.meta.maintainers or [ ]
-      ++ (flatten (map (x: x.members) (pkg.meta.teams or [ ]))));
+    elem
+      myMaintainer
+      (pkg.meta.maintainers or [ ] ++ (flatten (map (x: x.members) (pkg.meta.teams or [ ]))));
 
   isDerivationRobust = pkg:
     let
@@ -38,7 +39,8 @@ let
         isDerivation pkg
       );
     in
-    if result.success then result.value
+    if result.success then
+      result.value
     else false;
 
   brokenFilter = pkg:
@@ -46,7 +48,9 @@ let
       isBroken = pkg.meta.broken;
     in
     if showBroken then true
-    else if isBroken == false then true else false;
+    else if isBroken == false then
+      true
+    else false;
 
   isPkgSet = pkg:
     let
@@ -54,7 +58,8 @@ let
         (isAttrs pkg) && (pkg.recurseForDerivations or false)
       );
     in
-    if result.success then result.value
+    if result.success then
+      result.value
     else false;
 
   recursePackageSet = pkgSetName: pkgs:
@@ -73,5 +78,8 @@ in
 {
   packages =
     attrValues
-      (filterAttrsRecursive (n: v: v != null || v != { }) (recursePackageSet null pkgs));
+      (filterAttrsRecursive
+        (n: v: v != null || v != { })
+        (recursePackageSet null pkgs)
+      );
 }
